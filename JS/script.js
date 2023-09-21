79123ccdca36bf591ddbe39f65c2c5083190e129
franedsandi <franedsandi@gmail.com>
2023-09-21 18:38:56 +0200

    [mod] deck size and step by step in JS

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,9 @@
-
+/* 1. creare i testi in h1 e h2 
+2. organizare la informazione fornita in array come oggeti
+3. stampare ogni oggetto nella console per verificare la informazione
+4. con l'uso di bootstrap creare la card
+5. riempire la car con i datti generici e inserire i oggeti specifici dove vano */
+// 1.
 var h1 = document.createElement("h1");
 h1.textContent = "Our Team";
 
@@ -9,7 +14,7 @@ var container = document.querySelector(".container");
 
 container.appendChild(h1);
 container.appendChild(h2);
-
+// 2.
 const team =[
   {
   foto: 'wayne-barnett-founder-ceo.jpg',
@@ -46,9 +51,11 @@ const deck = document.getElementById('cardsdeck')
 deck.innerHTML += `<div id= "deck-manager" class="row row-cols-1 row-cols-md-3 mx-5 g-4"></div>`
 const manager = document.getElementById('deck-manager')
  for(let member of team){
+  // 3.
   console.log(member)
-  manager.innerHTML += `
-  <div class="col">
+  manager.innerHTML += 
+  // 4. 5.
+  `<div class="col">
     <div class="card border-0 rounded-0">
       <img src="./img/${member.foto}" class="card-img-top rounded-0" alt="...">
       <div class="card-body">
@@ -56,5 +63,5 @@ const manager = document.getElementById('deck-manager')
         <p class="card-text text-center">${member.role}</p>
       </div>
     </div>
-   `
+  </div>`
  }
